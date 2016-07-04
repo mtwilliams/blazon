60ad027bc186fd7c7a699014d69a438516ebe93f
Michael Williams <m.t.williams@live.com>
2016-07-04 12:32:42 -0700

    No need to wrap `Blazon.Serializers.JSON` as binding is at runtime.

--- lib/blazon/serializers/json.ex
+++ lib/blazon/serializers/json.ex
@@ -1,13 +1,11 @@
-if Code.ensure_loaded?(Poison) do
-  defmodule Blazon.Serializers.JSON do
-    @moduledoc ~S"""
-    """
+defmodule Blazon.Serializers.JSON do
+  @moduledoc ~S"""
+  """
 
-    @behaviour Blazon.Serializer
+  @behaviour Blazon.Serializer
 
-    def serialize(agnostic, opts) do
-      Enum.into(agnostic, %{})
-      |> Poison.encode!(Keyword.take(opts, ~w(pretty)a))
-    end
+  def serialize(agnostic, opts) do
+    Enum.into(agnostic, %{})
+    |> Poison.encode!(Keyword.take(opts, ~w(pretty)a))
   end
 end
